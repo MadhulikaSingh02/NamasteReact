14d7397ea191581b33fc01f1619eb00584e0cf16
Madhulika Singh <MadhulikaSingh02@gmail.com>
2024-05-21 14:22:33 -0700

    TYPE_KEY_MENU_ITEM added

--- 07-FindingThePath/src/utils/constants.js
+++ 07-FindingThePath/src/utils/constants.js
@@ -10,10 +10,14 @@ const RESTAURANT_MENU_API_URL =
   "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&catalog_qa=undefined&restaurantId=";
 const TYPE_KEY_RESTAURANT =
   "type.googleapis.com/swiggy.presentation.food.v2.Restaurant";
+const TYPE_KEY_MENU_ITEM =
+  "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+
 export {
   SWIGGY_APP_URL,
   IMAGE_CDN_URL,
   SHIMMER_CARD_COUNT,
   RESTAURANT_MENU_API_URL,
   TYPE_KEY_RESTAURANT,
+  TYPE_KEY_MENU_ITEM,
 };
