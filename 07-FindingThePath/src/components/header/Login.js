0a1c9d8796edc383aef73249ace7d4ea2a154106
Madhulika Singh <MadhulikaSingh02@gmail.com>
2024-05-22 10:53:02 -0700

    custom validate function

--- 07-FindingThePath/src/components/header/Login.js
+++ 07-FindingThePath/src/components/header/Login.js
@@ -8,15 +8,18 @@ Handling form submission*/
 import { useFormik } from "formik";
 
 function Login() {
-  console.log(useFormik({}));
+  // console.log(useFormik({}));
   // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
+  // be called when the form is submitted.
+  // Note that we have to initialize ALL of fields with values. These could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell at us.
   const formik = useFormik({
     initialValues: {
       email: "",
       firstName: "",
       lastName: "B",
     },
+    validate,
     onSubmit: (values) => {
       console.log(JSON.stringify(values));
     },
@@ -36,7 +39,7 @@ function Login() {
             value={formik.values.firstName}
           />
         </label>
-
+        {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : ""}
         <label htmlFor="lastName">
           <input
             id="lastName"
@@ -47,7 +50,7 @@ function Login() {
             value={formik.values.lastName}
           />
         </label>
-
+        {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : ""}
         <label htmlFor="email">
           <input
             id="email"
@@ -58,10 +61,35 @@ function Login() {
             value={formik.values.email}
           />
         </label>
-
+        {formik.errors.email ? <div>{formik.errors.email}</div> : ""}
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 }
+
+// A custom validation function. This must return an object whose keys are symmetrical to our values/initialValues
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length > 20) {
+    errors.lastName = "Must be 20 characters or less";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
+};
+
 export default Login;
