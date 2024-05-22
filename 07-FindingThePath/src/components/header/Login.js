c41196898835863dfd4aee046d9119009b87ef6c
Madhulika Singh <MadhulikaSingh02@gmail.com>
2024-05-22 10:38:49 -0700

    Formik - Initial

--- 07-FindingThePath/src/components/header/Login.js
+++ 07-FindingThePath/src/components/header/Login.js
@@ -1,43 +1,68 @@
+/*
+Formik is a small group of React components and hooks for building forms in React and React Native.
+It helps with the three most annoying parts:
+Getting values in and out of form state
+Validation and error messages
+Handling form submission*/
+
 import { useState } from "react";
+import { useFormik } from "formik";
 
 function Login() {
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    if (name === "input1") {
-      setInput1(value);
-    } else if (name === "input2") {
-      setInput2(value);
-    }
-  };
-
-  const handleSubmit = (event) => {
-    console.log("Inside handleSubmit", event);
-    event.preventDefault();
-  };
+  console.log(useFormik({}));
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      firstName: "",
+      lastName: "B",
+    },
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values));
+    },
+  });
 
   return (
     <div className="login-container login">
-      <form className="login-form" onSubmit={handleSubmit}>
+      <form className="login-form" onSubmit={formik.handleSubmit}>
         <span>Login</span>
-        <input
-          name="input1"
-          value={input1}
-          placeholder="Enter your first name"
-          onChange={handleInputChange}
-        />
-        <input
-          name="input2"
-          value={input2}
-          placeholder="Enter your last name"
-          onChange={handleInputChange}
-        />
+        <label htmlFor="firstName">
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            placeholder="Enter your first name"
+            onChange={formik.handleChange}
+            value={formik.values.firstName}
+          />
+        </label>
+
+        <label htmlFor="lastName">
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            placeholder="Enter your last name"
+            onChange={formik.handleChange}
+            value={formik.values.lastName}
+          />
+        </label>
+
+        <label htmlFor="email">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter your email id"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+        </label>
+
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 }
-
 export default Login;
